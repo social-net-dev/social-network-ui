39c2cd5436e9f6a1f72bf1746324590d20b32873
dung11122005 <dungakaishi900@gmail.com>
2026-01-29 21:50:58 +0700

    feat: post and register

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,26 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-        port: 5173,
+  },
+  server: {
+    port: 5173,
+  },
+  build: {
+    cssCodeSplit: false,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: () => "app",
+        entryFileNames: "app.js",
+        chunkFileNames: "app.js",
+        assetFileNames: "app.[ext]",
+      },
     },
+  },
 });
